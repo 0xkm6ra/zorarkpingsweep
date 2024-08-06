69f31265f833fab4d9bf878372e55f570785e8f7
K ㅀ <102567706+0xkm6ra@users.noreply.github.com>
2024-08-05 22:39:01 -0400

    first commit
    version 1.0

---
+++ zorarkpingsweep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "0XKM6RA - PING SWEEP"
+    echo "Modo de uso: $0 REDE"
+    echo "Exemplo: $0 192.168.0"
+else
+    for host in {1..254}
+    do
+        ping -c 1 $1.$host  | grep "64 bytes"  | cut -d ":" -f 1 | cut -d " " -f 4;
+    done
+fi
+
